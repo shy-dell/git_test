cf641b60e15f5d3ce92d982e09361f87b135940b
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-27 09:00:52 -0400

    update index src and practice func, also fizzbuzz

--- foundations_js/javascript.js
+++ foundations_js/javascript.js
@@ -1,24 +1,35 @@
-function ask(question, yes, no) {
-    if (confirm(question)) yes();
-    else no();
-  }
-  
-  ask(
-    "Do you agree?",
-    function() { console.log("You agreed."); },
-    function() { console.log("You canceled the execution."); }
-  )
+// function add7(num) {
+//     console.log(num + 7);
+// }
 
+// add7(7);
 
-//   The below does the same thing, but using a function 
-// expression would not allow a code block to be executed prior to the declaration
-  let ask = (question, yes, no) => {
-    confirm(question) ? yes():
-    no()
-  };
-  ask(
-    "Do you agree?",
-    () => console.log("You agreed."),
-    () => console.log("You canceled the execution.")
+// let multiply = (i,y) => console.log(i * y);
+// multiply(12,24);
 
-  );
+// let capitalize = x => console.log(x.charAt(0).toUpperCase() + x.slice(1))
+// capitalize("seven");
+
+// function lastLetter(word) {
+//     console.log(String(word).at(-1));
+// }
+// lastLetter("thsi is a longer sentence ending in z")
+
+function fizzBuzz(limit) {
+    for (let x = 1; x <= limit; x++) {
+        if (x % 3 == 0 && x % 5 == 0){
+            console.log('FizzBuzz');
+        }
+        else if (x % 3 == 0) {
+            console.log("Fizz");
+        }
+        else if (x % 5 == 0) {
+            console.log("Buzz")
+        }
+        else {
+            console.log(x)
+        }
+        }
+}
+
+fizzBuzz(parseInt(prompt("Please enter a number you would like FizzBuzzed: ")))
